0962b5f547cff0a9f25dfaec4e870878c3b90678
takayuki.kayawari <kayamelo@gmail.com>
2022-11-03 23:26:39 +0900

    Add mockデータ

--- http_server/models/models.go
+++ http_server/models/models.go
@@ -3,8 +3,8 @@ package models
 import "time"
 
 type Comment struct {
-	CommentId int       `json:"comment_id"`
-	ArticleId int       `json:"article_id"`
+	CommentID int       `json:"comment_id"`
+	ArticleID int       `json:"article_id"`
 	Message   string    `json:"message"`
 	CreatedAt time.Time `json:"created_at`
 }
